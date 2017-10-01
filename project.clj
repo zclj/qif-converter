862a95faa9bb9ccca8f5ffdeddae20bb83a5cb47
Stefan Karlsson <info-s@tryne.se>
2017-10-01 13:53:52 +0200

    Version bump

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/data.zip "0.1.0"]
-                 [org.clojure/tools.cli "0.2.1"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/data.zip "0.1.2"]
+                 [org.clojure/tools.cli "0.3.5"]]
   :main transactions.core)
